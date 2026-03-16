13c7aa486e0247b5fc46c44c24ba40ae7de22873
Anhtrr <Anhtrr7@gmail.com>
2026-03-15 22:21:39 -0400

    Change bundle ID to com.getjab.app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jab.app',
+  appId: 'com.getjab.app',
   appName: 'Jab',
   webDir: 'out',
 };
